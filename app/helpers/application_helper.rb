a2401fdde950237bcdc295565ca8bf285c2b3e4b
Dan Poggi <dan.poggi@gmail.com>
2011-03-26 03:43:39 -0400

    Wow, spent a lot of time on that stupid helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -12,7 +12,18 @@ module ApplicationHelper
   
   def slider_javascript_tag(options = {})
     slider_id = options[:id].is_a?(String) ? options[:id] : "slider"
-    js = "$(window).load(function() { $('##{slider_id}').nivoSlider(); });" 
+    
+    options.delete(:id) if not options[:id].nil?
+    options_js = if options.empty?
+                   ""
+                 else
+                   "{" + Array.new(options.keys.count) do
+                     pair = options.shift
+                     "#{pair.first}: #{pair.last.is_a?(String) ? "'#{pair.last}'" : pair.last}"
+                   end.join(", ") + "}"
+                 end
+
+    js = "$(window).load(function() { $('##{slider_id}').nivoSlider(#{options_js}); });" 
     
     yield js if block_given?
     javascript_tag js
