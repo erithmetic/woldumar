5cb436822e23bf1987c2e9c13021f58dcc54df84
Derek Kastner <dkastner@gmail.com>
2011-03-26 19:45:07 -0400

    Continuing registrations

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,10 +3,18 @@ class ApplicationController < ActionController::Base
 
   layout 'application'
 
-  private
+private
 
   def require_admin
     authenticate_user! unless current_user
     redirect_to root_path, :error => "You are not authorized." unless current_user.is_admin?
+  end
+
+  def store_location
+    session[:return_to] = request.fullpath
+  end
+
+  def clear_stored_location
+    session[:return_to] = nil
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
+require 'digest/sha1'
+
 class User < ActiveRecord::Base
+  has_many :event_registrations
+
   # Include default devise modules. Others available are:
   # :token_authenticatable, :encryptable, :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable, :recoverable, :rememberable, :trackable, :validatable
@@ -7,6 +11,10 @@ class User < ActiveRecord::Base
 
   # Setup accessible (or protected) attributes for your model
   # attr_accessible :email, :password, :password_confirmation, :remember_me
+  
+  def self.generate_password(email)
+    Digest::SHA1.hexdigest email
+  end
 
   def is_admin?
     is_admin
