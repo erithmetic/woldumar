60fdb760d9ce4736af363b303bd4fbfc6f238e18
Dan Poggi <dan.poggi@gmail.com>
2011-03-26 02:43:32 -0400

    Added helpers for image slider

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,15 @@
 module ApplicationHelper
+  def slider_image_tags
+    image_tags = Dir.glob("#{Rails.root}/public/images/slider/*").map do |filename|
+      <<-EOF
+<img src="/#{filename.split(/\//)[-3..-1].join("/")}" alt="">
+      EOF
+    end.join("")
+
+    raw image_tags
+  end
+  
+  def slider_javascript_tag(slider_id)
+    javascript_tag "$(window).load(function() { $('##{slider_id}').nivoSlider(); });" 
+  end
 end
