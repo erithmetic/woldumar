26fe928d0b14c4acab103f56104ef8cd52b262db
Jimmy Perrine <jimmy.perrine@gmail.com>
2011-03-26 19:58:42 -0400

    Finished form, added code for create to repopulate the sessions list

--- app/controllers/session_registrations_controller.rb
+++ app/controllers/session_registrations_controller.rb
@@ -44,6 +44,7 @@ class SessionRegistrationsController < ApplicationController
   # POST /session_registrations
   # POST /session_registrations.xml
   def create
+    @sessions = Session.all
     @session_registration = current_user.session_registrations.new(params[:session_registration])
 
     respond_to do |format|
