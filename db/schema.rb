ee99845c24640ed2845dc1da78ae3770fe982d3b
Bradley Handy <brad.handy@gmail.com>
2011-03-25 21:47:09 -0400

    Added the start and end dates and the details to the event model.

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,16 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110326005406) do
+ActiveRecord::Schema.define(:version => 20110326010651) do
 
   create_table "events", :force => true do |t|
     t.string   "name"
     t.string   "description"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.text     "details"
+    t.datetime "start_date"
+    t.datetime "end_date"
   end
 
 end
