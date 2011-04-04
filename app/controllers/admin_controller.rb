fae95eacdc816db3e4c7e201e53c0b91b96feb21
Derek Kastner <dkastner@gmail.com>
2011-04-03 21:46:47 -0400

    Users are searchable/paged

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -5,7 +5,11 @@ class AdminController < ApplicationController
   end
   
   def users
-    @users = User.all
+    if params[:search]
+      @users = User.search(params[:search], params[:page])
+    else
+      @users = User.paginate :page => params[:page], :order => 'last_name, first_name DESC'
+    end
   end
   
   def edit_user
@@ -14,6 +18,12 @@ class AdminController < ApplicationController
   
   def update_user
     @user = User.find(params[:id])
+
+    unless params[:user][:password].present?
+      params[:user][:password_confirmation] = nil
+      params[:user][:password_confirmation] = nil
+    end
+
     if @user.update_attributes(params[:user])
       redirect_to url_for(:action => :users), :notice => 'User was successfully updated'
     else
